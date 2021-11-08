de7168436a5ed67a88273c95dc604fbca88c2b1f
Brian Utterback <brian@utterback.org>
2021-11-08 07:24:40 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import animals.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        MammalInt m = new MammalInt();
+        m.eat();
+        m.travel();
+    }
+}
---
+++ src/animals/MammalInt.java
@@ -0,0 +1,14 @@
+package animals;
+
+public class MammalInt implements Animal {
+    public void eat() {
+        System.out.println("Mammal eats");
+    }
+    public void travel() {
+        System.out.println("Mammal travels");
+    }
+    public int noOfLegs() {
+        return 0;
+    }
+
+}
